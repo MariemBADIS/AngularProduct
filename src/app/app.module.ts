0daedac2223f5288c1efdcd1e8f5f94a1392f6fb
MariemBADIS <mariem.badis@rhis-solutions.com>
2020-05-17 13:25:49 +0100

    feat : configuration-routing__add_product-detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,10 @@ import {AppComponent} from './app.component';
 import {HomeComponent} from './home/home.component';
 import {ProductListComponent} from './products/product-list/product-list.component';
 import {ConvertToSpacesPipe} from './shared/convert-to-spaces.pipe';
-import { StarComponent } from './shared/star/star.component';
+import {StarComponent} from './shared/star/star.component';
 import {HttpClientModule} from '@angular/common/http';
+import {ProductDetailComponent} from './products/product-details/product-detail.component';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -15,12 +17,20 @@ import {HttpClientModule} from '@angular/common/http';
     HomeComponent,
     ProductListComponent,
     ConvertToSpacesPipe,
-    StarComponent
+    StarComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: 'products', component: ProductListComponent },
+      { path: 'products/:id', component: ProductDetailComponent },
+      { path: 'home', component: HomeComponent },
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: '**', component: HomeComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/products/product-details/product-detail.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+import {IProduct} from '../product';
+
+@Component({
+  selector: 'app-product-detail',
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.scss']
+})
+export class ProductDetailComponent implements OnInit {
+
+  pageTitle = 'Product Detail';
+
+  product: IProduct;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
